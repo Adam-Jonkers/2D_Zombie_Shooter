eb413a43116754ec59367824c6fe0fc4b0d4f71a
Adam Jonkers <ajo164@uclive.ac.nz>
2023-01-18 18:08:14 +1300

    add entitys to Game and Global Structs

--- RPG_GAME.h
+++ RPG_GAME.h
@@ -1,12 +1,46 @@
 #ifndef RPG_GAME_H
 #define RPG_GAME_H
 
-typedef struct {
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
+#include "CORE.h"
+#include "PLAYER.h"
+
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Surface* icon;
+    vec2_t windowSize;
+    int window_width;
+    int window_height;
+    mouse_t mouse;
+    TTF_Font* font;
+    Text_t fpsText;
+    Timer_t fpsTimer;
+    Timer_t stepTimer;
+    float dt;
+    const Uint8* keyboard_state;
 } Global_t;
 
 typedef struct {
-
+    vec2_t max;
+    SDL_Texture* loadingScreen;
+    Player_t player;
+    Enemys_t enemys;
+    float* noiseMap;
+    float* randArray; 
+    char* map; // is this actually needed?
+    bitmap_t mapBitmap;
+    Text_t playerHealthText;
+    Score_t score;
+    FILE* highscoreFile;
+    SDL_Texture* mapTexture;
+    Timer_t enemySpawnTimer;
+    Timer_t difficultyTimer;
+    u_int32_t difficultyIncreaseRate;
+    u_int32_t spawnRate;
 } Game_t;
 
 Global_t Setup_Global(void);
